1aa31be2cd80021f23e99905f9d3bd0e56876d5a
rahmaniaam <rahmaniaam26@gmail.com>
2018-02-20 11:36:48 +0700

    redirect

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 Nih DUDU dari aku untuk kamu ea
 
 Rahmania Astrid Mochtar (aci)
+
+cara make local:
+1. install python
+2. install pip
+3. install virtualenv
+4. jalankan virtualenv
+5. pip install requirements.txt
+6. python manage.py runserver
+7. bila styling tidak ke load, jalankan python manage.py collectstatic
--- dudu/urls.py
+++ dudu/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('message/', include('message.urls', namespace='message')),
+    path('', RedirectView.as_view(permanent=True, url='/message/'), name='index')
 ]
