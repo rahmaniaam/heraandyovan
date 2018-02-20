09748da84cdaff2947653370c97ae9d5bb6b60ed
rahmaniaam <rahmaniaam26@gmail.com>
2018-02-20 09:05:10 +0700

    tests

--- message/tests.py
+++ message/tests.py
@@ -2,3 +2,6 @@ from django.test import TestCase
 
 # Create your tests here.
 class Dudu(TestCase):
+	def test_index_url_is_exist(self):
+        response = Client().get('/message/')
+        self.assertEqual(response.status_code, 200)
