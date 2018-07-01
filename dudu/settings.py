66785439a9c3b25b7c51f7fcc99e1ac84e2c2840
rahmaniaam <rahmaniaam26@gmail.com>
2018-07-01 22:03:23 +0700

    settings

--- dudu/settings.py
+++ dudu/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'message',
-    'reception',
-    'livereload'
+    'reception'
+    #'livereload'
 ]
 
 MIDDLEWARE = [
