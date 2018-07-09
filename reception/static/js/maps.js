2fa7488947f02b15dfa3a2df60421f5641db8899
rahmaniaam <rahmaniaam26@gmail.com>
2018-07-09 11:28:40 +0700

    pqwoqpow

--- reception/static/js/maps.js
+++ reception/static/js/maps.js
@@ -1,7 +1,7 @@
 function initMap() {
     const bidakara = {
-        lat: -6.2401104,
-        lng: 106.8408638
+        lat: -6.240516,
+        lng: 106.8415298
     }
     const map = new google.maps.Map(document.getElementById('map'), { zoom: 17, center: bidakara });
     const marker = new google.maps.Marker({ position: bidakara, map: map, animation: google.maps.Animation.BOUNCE });
